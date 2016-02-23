8a7a17966122399e5a75976323d0e65b73b5ca47
TPDev.Raphael/ <raphael@tpdev.at>
2016-02-23 13:26:23 +0100

    MessageFactory => Added Merge.cmd / NOT READY YET!

---
+++ Merge.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe" /ndebug /copyattrs /targetplatform:4.0,"C:\Windows\Microsoft.NET\Framework64\v4.0.30319" /out:TPDev.MessageFactory.dll TPDev.SmsFactory\bin\Release\TPDev.SmsFactory.dll TPDev.SmsFactory\bin\Release\TPDev.SmsInterface.dll DbFactory\bin\Release\DbLogger.dll DbFactory\bin\Release\MySQLLibrary.dll DbFactory\bin\Release\SQLiteLibrary.dll DbFactory\bin\Release\SQLLibrary.dll
+pause

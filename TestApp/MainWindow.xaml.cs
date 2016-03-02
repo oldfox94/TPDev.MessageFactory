6222bec0ea0fea9e251731e0f9804a5dda7df372
oldfox94 <raphael@tpdev.theproducers.at>
2016-03-02 22:18:10 +0100

    MessageFactory: - TestApp hinzugefügt.

---
+++ TestApp/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows;
+using TPDev.SmsFactory;
+using TPDev.SmsInterface.Models;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SmsFactory m_smsFactory { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnLoaded;
+
+            var connectionData = new SmsConnectionData();
+
+            //WebSMS
+            //connectionData.User = "";
+            //connectionData.Password = "";
+
+            //m_smsFactory = new SmsFactory(SmsType.WebSMS, connectionData);
+
+            //SmsApi
+            connectionData.User = "";
+            connectionData.Password = ""; //You can get your MD5 Passwort @SmsApi Account Page
+
+            m_smsFactory = new SmsFactory(SmsType.SmsApi, connectionData);
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            txtMessage.Text = "This is a test sms from TPDev.MessageFactory!";
+        }
+
+        private void OnSendSmsClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtPhoneNr.Text)) return;
+            if (string.IsNullOrEmpty(txtMessage.Text)) return;
+
+            long phoneNum = 0;
+            long.TryParse(txtPhoneNr.Text, out phoneNum);
+
+            var result = m_smsFactory.Send.Send(phoneNum, txtMessage.Text, 0, true);
+        }
+    }
+}

9c95fd95d9f660ed356179353e490fd05df12652
oldfox94 <raphael@tpdev.theproducers.at>
2016-03-12 10:16:51 +0100

    MessageFactory: - RELEASE V1.0.0.1

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
 [assembly: AssemblyDescription("Beta")] // Testversion, RC1, RC2,  Rev. 1,

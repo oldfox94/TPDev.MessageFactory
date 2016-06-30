06aec365200e58ab3be4c4aa4ac9a99172270dfb
tpDev <raphael@tpdev.at>
2016-06-30 15:15:14 +0200

    TPDev.MessageFactory: - GlobalAssembly anpassungen

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -13,6 +13,6 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.3")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
 [assembly: AssemblyDescription("Beta")] // Testversion, RC1, RC2,  Rev. 1,
